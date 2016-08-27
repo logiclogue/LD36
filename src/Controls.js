c05a08c679313c9891b47bfb0eec5b12b1655472
logiclogue <jordanpaullord@hotmail.co.uk>
2016-08-27 15:00:14 +0100

    Adds Constrols class

---
+++ src/Controls.js
@@ -0,0 +1,58 @@
+function Controls() {
+    this.keysDown = [];
+    this.keys = {
+        left: 37,
+        right: 39,
+        up: 28,
+        down: 40,
+        w: 87,
+        a: 65,
+        s: 83,
+        d: 68,
+        enter: 13,
+        space: 32,
+        m: 77,
+        z: 90,
+        p: 80
+    };
+
+    this._keydownFunc = this._keydown.bind(this);
+    this._keyupFunc = this._keyup.bind(this);
+}
+
+(function (proto_) {
+
+    /*
+     * Enables the event listeners.
+     */
+    proto_.enable = function () {
+        addEventListener('keydown', this._keydown, false);
+        addEventListener('keyup', this._keyup, false);
+    };
+
+    /*
+     * Disables the event listeners.
+     */
+    proto_.disable = function () {
+        removeEventListener('keydown', this._keydown);
+        removeEventListener('keyup', this._keyup);
+    };
+
+
+    /*
+     * When key is down, value at the index of the key code is true.
+     */
+    proto_._keydown = function (e) {
+        this.keysDown[e.keyCode] = true;
+    };
+
+    /*
+     * When key is up, value at the index of the key code is undefined.
+     */
+    proto_._keyup = function (e) {
+        delete this.keysDown[e.keyCode];
+    }
+
+}(Controls.prototype));
+
+module.exports = Controls;
